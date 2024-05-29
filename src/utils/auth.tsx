a97ea7c91627a7702fdd7d6916b246bda1f8e399
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-29 22:16:40 +0700

    feat: add auth

---
+++ src/utils/auth.tsx
@@ -0,0 +1,14 @@
+import { getToken } from "./token";
+
+export const withAuth = (): string | null => {
+  let token;
+
+  if (getToken() == null) {
+    console.log("login dulu");
+    return null;
+  } else {
+    token = getToken();
+    console.log(`Ini token mu : `, token);
+    return token;
+  }
+};
