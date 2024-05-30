a771d4587d269896c03ce9b1a3dc0288729ed029
Rafi Sutrisno <mrafis2003@gmail.com>
2024-05-30 14:02:05 +0700

    feat: fix prediciton page

--- src/fetch/getUser.tsx
+++ src/fetch/getUser.tsx
@@ -1,14 +1,18 @@
 import { baseApi } from "@/utils/baseApi";
 import { getToken } from "@/utils/token";
-import { useState } from "react";
 
 export async function GetUser() {
-  //   console.log(getToken());
-  const response = await baseApi.get(`/user/me`, {
-    headers: {
-      Authorization: `Bearer ${getToken()}`,
-    },
-  });
-  //   console.log(response.data);
-  return response.data;
+  try {
+    const token = getToken();
+
+    const response = await baseApi.get(`/user/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch user data:", error);
+    throw error;
+  }
 }
