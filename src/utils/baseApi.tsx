547b59a5809a8f42de3381b2d9f6358b0fc60452
Ahdaaa <afilzag@gmail.com>
2024-06-02 23:19:40 +0700

    change: timeout api call

--- src/utils/baseApi.tsx
+++ src/utils/baseApi.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseApi = axios.create({
   baseURL: "http://localhost:8080",
-  timeout: 5000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
