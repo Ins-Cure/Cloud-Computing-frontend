d7f567660acf8f257e60c7cf9b1fc3dbf9f81649
Rafi Sutrisno <mrafis2003@gmail.com>
2024-06-12 00:22:53 +0700

    feat: add delete prediction fetch

--- src/fetch/deletePredictionbyId.tsx
+++ src/fetch/deletePredictionbyId.tsx
@@ -1,15 +1,22 @@
 import { baseApi } from "@/utils/baseApi";
 import { getToken } from "@/utils/token";
 
-export async function deletePredictionbyID(predictId: string) {
+export async function deletePredictionbyID(
+  predictId: string,
+  predictLink: string
+) {
   try {
     const token = getToken();
+    predictLink = predictLink.split("/").pop() || "";
 
-    const response = await baseApi.get(`/prediction/del/${predictId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await baseApi.delete(
+      `/prediction/del/${predictId}/${predictLink}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Failed to fetch user data:", error);
