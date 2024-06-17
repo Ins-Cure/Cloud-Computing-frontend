ace6f576b313bd0f32354ba68b894de7dbc2ec1b
Ahdaaa <afilzag@gmail.com>
2024-06-17 23:08:23 +0700

    add: new protocol for gdrive images

--- next.config.mjs
+++ next.config.mjs
@@ -36,6 +36,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
