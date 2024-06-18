2dd65b67b17c60a315f20d28b0327d75178ee0c1
Rafi Sutrisno <mrafis2003@gmail.com>
2024-06-18 13:32:25 +0700

    fix: postRegister fetch

--- src/fetch/postRegister.tsx
+++ src/fetch/postRegister.tsx
@@ -1,10 +1,12 @@
 import { baseApi } from "@/utils/baseApi";
+import { NextRouter } from "next/router";
 
-function handlefetch(
+async function handlefetch(
   name: string,
   email: string,
   notelp: string,
-  pass: string
+  pass: string,
+  router: NextRouter
 ) {
   const data = {
     name: name,
@@ -13,11 +15,14 @@ function handlefetch(
     pass: pass,
   };
   // console.log(data);
-  postRegister(data);
+  postRegister(data, router);
   // Configure the fetch request with method, headers, and body
 }
 
-export function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+export async function handleSubmit(
+  event: React.FormEvent<HTMLFormElement>,
+  router: NextRouter
+) {
   event.preventDefault(); // Prevent default form submission behavior
   const formData = new FormData(event.currentTarget); // Get form data
 
@@ -32,7 +37,7 @@ export function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     typeof name === "string" &&
     typeof notelp === "string"
   ) {
-    handlefetch(name, email, notelp, password); // Call handleFetch with form input values
+    handlefetch(name, email, notelp, password, router); // Call handleFetch with form input values
   } else {
     // Handle the case where email or password is null or not a string
     console.error("Email or password is missing or not a string");
@@ -41,7 +46,8 @@ export function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
   event.currentTarget.reset();
 }
 
-export async function postRegister(data: object) {
+export async function postRegister(data: object, router: NextRouter) {
   const response = await baseApi.post(`/inscure/add`, data);
   console.log(response.data.message);
+  router.push("/login");
 }
