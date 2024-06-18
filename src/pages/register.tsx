bbe387ef14883051d02c808f563340f3ea6f2e01
Rafi Sutrisno <mrafis2003@gmail.com>
2024-06-18 13:30:41 +0700

    feat: styling register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,40 +9,47 @@ const RegisterUser = () => {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen w-full flex justify-center items-center bg-gray-100 p-5">
+      <div className="min-h-fit w-full flex justify-center  bg-gray-100 p-5">
         <form
-          onSubmit={handleSubmit}
-          className="formRegister flex flex-col gap-5 bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-black"
+          onSubmit={(e) => {
+            handleSubmit(e, router);
+          }}
+          className="formRegister flex flex-col gap-5 bg-white p-8 rounded-lg shadow-lg shadow-purple-200 w-full max-w-md text-black"
         >
-          <h2 className="text-2xl font-bold text-gray-800">Register Page</h2>
+          <h2 className="text-2xl font-bold text-purple-800 self-center">
+            Register
+          </h2>
           <input
             type="text"
             name="name"
             placeholder="Name"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-500"
           />
           <input
-            type="text"
+            type="email"
             name="email"
             placeholder="Email"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-500"
           />
           <input
             type="text"
             name="notelp"
             placeholder="Phone Number"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-500"
           />
           <input
             type="password"
             name="password"
             placeholder="Password"
-            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-purple-500"
           />
           <button
             type="submit"
-            className="button-register p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300"
-            onClick={() => router.push("/login")}
+            className="p-3 bg-purple-600 text-white rounded-lg hover:bg-purple-500 transition duration-300 font-semibold"
           >
             Submit
           </button>
